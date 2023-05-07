f9d0ee19f2279ec44dab068564e23262bc656717
MakeevAlexander <Pipifax@bk.ru>
2023-05-07 21:45:03 +0300

    finish

--- src/test/java/ru/netology/qa/ManagerFilmsTest.java
+++ src/test/java/ru/netology/qa/ManagerFilmsTest.java
@@ -81,5 +81,69 @@ public class ManagerFilmsTest {
         String[] expected = {film12, film11, film10, film9, film8, film7, film6,film5,film4,film3,film2,film1,};
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void test4() {
+        String film1 = "Brat";
+        String film2 = "Brat2";
+        String film3 = "Brat3";
+        String film4 = "Brat4";
+        String film5 = "Brat5";
+        String film6 = "Brat6";
+        String film7 = "Brat7";
+        String film8 = "Brat8";
+        String film9 = "Brat9";
+        String film10 = "Brat10";
+        String film11 = "Brat11";
+        String film12 = "Brat12";
+        ManagerFilms manager = new ManagerFilms(13);
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+        manager.addFilm(film11);
+        manager.addFilm(film12);
+
+        String[] actual = manager.lostFilm();
+        String[] expected = {film12, film11, film10, film9, film8, film7, film6,film5,film4,film3,film2,film1,};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void test5() {
+        String film1 = "Brat";
+        String film2 = "Brat2";
+        String film3 = "Brat3";
+        String film4 = "Brat4";
+        String film5 = "Brat5";
+        String film6 = "Brat6";
+        String film7 = "Brat7";
+        String film8 = "Brat8";
+        String film9 = "Brat9";
+        String film10 = "Brat10";
+        String film11 = "Brat11";
+        String film12 = "Brat12";
+        ManagerFilms manager = new ManagerFilms(2);
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+        manager.addFilm(film11);
+        manager.addFilm(film12);
+
+        String[] actual = manager.lostFilm();
+        String[] expected = {film12, film11};
+        Assertions.assertArrayEquals(expected, actual);
+    }
 
 }
